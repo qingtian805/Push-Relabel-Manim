bde5724e980e1c7511544421a980eda5bfbd6a10
Kevin Li <qingtian805@qq.com>
2025-06-03 13:30:10 +0800

    finish residual net animation

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def update_dot_fill(dot:LabeledDot, color, opacity):
     dot.set_fill(color, opacity)
     dot.submobjects[0].set_fill(WHITE, opacity=1)
 
-def animate_flow(scene:Scene, quantity, start: LabeledDot, end: LabeledDot, edge, capcity, used):
+def animate_flow(scene:Scene, quantity, start: LabeledDot, end: LabeledDot, capcity, used, edge):
     data: list[Square] = []
     for i in range(quantity):
         data.append(Square(side_length=0.3, color=BLUE).move_to(start))
-    
+
     new_edge = LabeledArrow(label=MathTex(f"{used}/{capcity}"))
     update_arrow(new_edge, start, end)
 
@@ -29,45 +29,63 @@ def animate_flow(scene:Scene, quantity, start: LabeledDot, end: LabeledDot, edge
         )
     )
     scene.remove(*data)
-    edge = new_edge
-    scene.add(edge)
-    scene.remove(new_edge)
+    # new_edge.to_corner()
+    scene.add(new_edge)
+    scene.remove(edge)
 
-def animate_flow_parallel(scene:Scene, quantity, start: LabeledDot, end: LabeledDot, edge, capcity, used, parallel_edge=None):
+    return new_edge
+
+def animate_flow_parallel(scene:Scene, quantity, start: LabeledDot, end: LabeledDot, capcity, used, edge=None, parallel_edge=None):
+    def create_parallel():
+        new_parallel = LabeledArrow(label=MathTex(f"{used}"))
+        update_arrow(new_parallel, end.get_corner(DL), start.get_corner(DR))
+        return new_parallel
+
+    def create_edge():
+        new_edge = LabeledArrow(label=MathTex(f"{capcity - used}"))
+        update_arrow(new_edge, start.get_corner(UR), end.get_corner(UL))
+        return new_edge
+
     data: list[Square] = []
     for i in range(quantity):
         data.append(Square(side_length=0.3, color=BLUE).move_to(start))
-    
-    if allow_parallel and capcity - used < capcity and capcity - used > 0:
-        new_edge = LabeledArrow(label=MathTex(f"{capcity - used}"))
-        parallel_edge = LabeledArrow(label=MathTex(f"{used}"))
-        update_arrow(new_edge, start.get_corner(UR), end.get_corner(UL))
-        update_arrow(parallel_edge, end.get_corner(DL), start.get_corner(DR))
-    elif allow_parallel and capcity - used == capcity:
-        new_edge = LabeledArrow(label=MathTex(f"{capcity}"))
-        
-    animations = [
+
+    new_edge = None
+    new_parallel = None
+    # only under case used == capcity edge is not used
+    if used < capcity:
+        new_edge = create_edge()
+    # only under case used == 0 parallel edge is not used
+    if used > 0:
+        new_parallel = create_parallel()
+
+    data_animations = [
         mob.animate.move_to(end)
         for mob in data
     ]
-    if allow_parallel:
-        scene.play(AnimationGroup(
-            Transform(edge, new_edge),
-            Transform(parallel_edge, )
-            AnimationGroup(*animations, lag_ratio=0.3),
-            )
+    edge_animations = []
+    new_edges = []
+
+    for i, t in [(edge, new_edge), (parallel_edge, new_parallel)]:
+        if i is None:
+            edge_animations.append(Write(t))
+            new_edges.append(t)
+        elif t is None:
+            edge_animations.append(FadeOut(i))
+        else:
+            edge_animations.append(Transform(i, t))
+            new_edges.append(t)
+
+    scene.play(AnimationGroup(
+        *edge_animations,
+        AnimationGroup(*data_animations, lag_ratio=0.3),
         )
-    else:
-        scene.play(AnimationGroup(
-            Transform(edge, new_edge),
-            AnimationGroup(*animations, lag_ratio=0.3),
-            )
-        )
+    )
+    scene.add(*new_edges)
+    scene.remove(edge, parallel_edge)
     scene.remove(*data)
-    
-    
-    scene.add(flow)
-    scene.remove(new_flow)
+
+    return new_edge, new_parallel
 
 def init_flow_net():
     # ---Init Flow_Net---
@@ -199,115 +217,27 @@ class Residual_Net(Scene):
 
         self.play(Write(source), Write(target), Write(flow))
 
-        # ---Make Residual_Net---
+        # ---Make Simple Residual_Net---
         res_source = source.copy()
         res_target = target.copy()
         res_flow   = flow.copy()
-        
+
         flow_net = VGroup(source, target, flow)
         res_net  = VGroup(res_source, res_target, res_flow).move_to((32/9, 0, 0))
         self.play(flow_net.animate.move_to((-32/9, 0, 0), ))
         self.play(Write(res_net))
 
         # ---Aminate Flow on Flow net---
-        data: list[Square] = []
-        for i in range(4):
-            data.append(Square(side_length=0.3, color=BLUE).move_to(source))
-
-        new_flow = LabeledArrow(label=MathTex("4/8"))
-        update_arrow(new_flow, source, target)
-        
-        self.play(AnimationGroup(
-            AnimationGroup(data[0].animate.move_to(target),
-                           Transform(flow, new_flow)),
-            data[1].animate.move_to(target),
-            data[2].animate.move_to(target),
-            data[3].animate.move_to(target),
-            lag_ratio=0.2
-        ))
-        self.remove(*data)
-        del flow
-        flow = new_flow
-        self.add(flow)
-        self.remove(new_flow)
+        flow = animate_flow(self, 4, source, target, 8, 4, flow)
 
         # ---Aminate Flow on Residual net---
-        for i in data:
-            i.move_to(res_source)
-
-        new_flow = LabeledArrow(label=MathTex("4"))
-        update_arrow(new_flow, res_source.get_corner(DR), res_target.get_corner(DL))
-
-        parallel_edge = LabeledArrow(label=MathTex("4"))
-        update_arrow(parallel_edge, res_target.get_corner(UL), res_source.get_corner(UR))
-        
-        self.play(AnimationGroup(
-            AnimationGroup(data[0].animate.move_to(res_target),
-                           Transform(res_flow, new_flow),
-                           Write(parallel_edge)),
-            data[1].animate.move_to(res_target),
-            data[2].animate.move_to(res_target),
-            data[3].animate.move_to(res_target),
-            lag_ratio=0.2
-        ))
-        self.remove(*data)
-        self.remove(res_flow)
-        del res_flow
-        res_flow = new_flow
+        res_flow, res_parallew = animate_flow_parallel(self, 4, res_source, res_target, 8, 4, res_flow, None)
 
         # ---Aminate Full Flow on Flow net ---
-        data: list[Square] = []
-        for i in range(4):
-            data.append(Square(side_length=0.3, color=BLUE).move_to(source))
-
-        new_flow = LabeledArrow(label=MathTex("4/8"))
-        update_arrow(new_flow, source, target)
-        
-        self.play(AnimationGroup(
-            AnimationGroup(data[0].animate.move_to(target),
-                           Transform(flow, new_flow)),
-            data[1].animate.move_to(target),
-            data[2].animate.move_to(target),
-            data[3].animate.move_to(target),
-            lag_ratio=0.2
-        ))
-        self.remove(*data)
-        del flow
-        flow = new_flow
-        self.remove(new_flow)
-        self.add(flow)
+        flow = animate_flow(self, 4, source, target, 8, 8, flow)
 
         # ---Aminate Full Flow on Residual net---
-        for i in data:
-            i.move_to(res_source)
-
-        new_flow = LabeledArrow(label=MathTex("4"))
-        update_arrow(new_flow, res_source.get_corner(DR), res_target.get_corner(DL))
-
-        parallel_edge = LabeledArrow(label=MathTex("4"))
-        update_arrow(parallel_edge, res_target.get_corner(UL), res_source.get_corner(UR))
-        
-        self.play(AnimationGroup(
-            AnimationGroup(data[0].animate.move_to(res_target),
-                           Transform(res_flow, new_flow),
-                           Write(parallel_edge)),
-            data[1].animate.move_to(res_target),
-            data[2].animate.move_to(res_target),
-            data[3].animate.move_to(res_target),
-            lag_ratio=0.2
-        ))
-        self.remove(*data)
-        del res_flow
-        res_flow = new_flow
-        self.remove(res_flow)
-
-        
-
-        
-        
-        
-
-        
+        res_flow, res_parallew = animate_flow_parallel(self, 4, res_source, res_target, 8, 8, res_flow, res_parallew)
 
 class Push_Relabel(Scene):
     def construct(self):
@@ -322,9 +252,14 @@ class Push_Relabel(Scene):
         self.play(title.animate.scale(0.5))
         self.play(title.animate.to_edge(UP))
 
+        subtitle = Text("基本操作").scale(0.8).scale(2)
+        self.play(Write(subtitle))
+        self.wait()
+        self.play(subtitle.animate.scale(0.5))
+        self.play(subtitle.animate.next_to(title))
+
         flow_net = VGroup(*vertices.values(), *edges.values())
         self.play(Write(flow_net))
-        
 
 
 class test(Scene):
